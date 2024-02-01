f1aad0c0c89afa2a8a5c82be819bf0581bbe4854
ZeeptfSpoon <149878210+ZeeptfSpoon@users.noreply.github.com>
2024-02-01 01:21:50 +0100

    Update fastAPI.py

--- fastAPI.py
+++ fastAPI.py
@@ -12,3 +12,11 @@ async def root():
 @app.get("/item/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/users/me")
+async def read_item():
+    return {"user_id": "The current user"}
+
+@app.get("/users/{user_id}")
+async def read_item(user_id: str, q: Optional[str] = None):
+    return {"user_id": user_id, "q": q}
